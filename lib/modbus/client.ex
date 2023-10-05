4914a54dea96ead4a7eda59530614de5f475a631
ermanimer <ermanimer@gmail.com>
2023-10-06 00:27:09 +0300

    Remove unnecessary function parameters

--- lib/modbus/client.ex
+++ lib/modbus/client.ex
@@ -18,14 +18,20 @@ defmodule Modbus.Client do
   end
 
   defp send_request(socket, request) do
-    {:gen_tcp.send(socket, request), socket}
+    case :gen_tcp.send(socket, request) do
+      :ok ->
+        {:ok, socket}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 
   defp receive_response({:ok, socket}, timeout) do
     :gen_tcp.recv(socket, 0, timeout)
   end
 
-  defp receive_response({{:error, reason}, _}, _), do: {:error, reason}
+  defp receive_response({:error, reason}, _timeout), do: {:error, reason}
 
   defp handle_response({:ok, response}) do
     case {err_code, exc_code} = extract_read_err_codes(response) do
@@ -33,7 +39,7 @@ defmodule Modbus.Client do
         {:ok, response}
 
       _ ->
-        {:error, %{:err_code => err_code, :exc_code => exc_code}}
+        {:error, "error code: #{err_code} exception_code: #{exc_code}"}
     end
   end
 
